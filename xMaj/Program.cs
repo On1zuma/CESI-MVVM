203e327e895fa9eaa926e9db167a42cfc1cfc4d0
On1zuma <wassi.mess@hotmail.com>
2024-02-05 09:40:28 +0100

    first commit

---
+++ xMaj/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xMaj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to xMaj");
+            Console.WriteLine("Entrez un minimum de 1 à 8 caractères:");
+            Console.ReadLine();
+        }
+    }
+}
